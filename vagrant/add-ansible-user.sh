f423e41235a5b651e9519c7ac4a9fbc3a3a80088
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2018-10-24 00:30:52 +0200

    Fix Synced Folder and ansible user provisioning order

--- vagrant/add-ansible-user.sh
+++ vagrant/add-ansible-user.sh
@@ -1,4 +1,6 @@
+rm -rf /home/ansible/.ssh
 mkdir -p /home/ansible/.ssh
-useradd -d /home/ansible -s /bin/bash -c "Ansible user" ansible 
+groupadd -f -g 1002 ansible
+useradd -d /home/ansible -s /bin/bash -c "Ansible user" -u 1002 -g 1002 ansible 
 chown -R ansible:ansible /home/ansible
 echo "ansible ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ansible
