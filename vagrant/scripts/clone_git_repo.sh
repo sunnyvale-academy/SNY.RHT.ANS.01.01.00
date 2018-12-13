ac202ec8de29a8af5e48681a72fb6290acd5396d
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2018-12-13 13:02:48 +0100

    Housekeeping

---
+++ vagrant/scripts/clone_git_repo.sh
@@ -0,0 +1,2 @@
+sudo -u ansible mkdir /home/ansible/git
+sudo -u ansible -H git clone https://github.com/sunnyvale-academy/SNY.OSS.ANS.01.01.00.git /home/ansible/git/SNY.OSS.ANS.01.01.00
--- vagrant/scripts/install_ansible.sh
+++ vagrant/scripts/install_ansible.sh
@@ -1,5 +1,3 @@
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
 sudo apt-get install software-properties-common
 sudo apt-add-repository --yes ppa:ansible/ansible-2.7
 sudo apt-get update
