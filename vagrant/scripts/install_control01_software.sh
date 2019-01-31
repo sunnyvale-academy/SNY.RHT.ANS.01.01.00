67d334a16628fa33600197361efb08057c2ecc3f
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2019-01-31 01:28:49 +0100

    Added Vagrant provisioner scripts/install_control01_software.sh

---
+++ vagrant/scripts/install_control01_software.sh
@@ -0,0 +1,7 @@
+sudo apt-get install tree -y
+sudo apt-get install curl -y 
+sudo add-apt-repository ppa:fkrull/deadsnakes-python2.7 -y
+sudo apt-get install python2.7 -y
+sudo apt-get install python-pip -y
+sudo pip install docker 
+sudo pip install 'docker-compose>=1.7.0'
