b44b9e1222ef4d83b88c88e448b69988f8d3102f
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-01-21 22:45:21 +0100

    fixes

--- vagrant/scripts/install_control01_software.sh
+++ vagrant/scripts/install_control01_software.sh
@@ -1,6 +1,7 @@
 sudo apt-get install tree -y
 sudo apt-get install curl -y 
 sudo add-apt-repository ppa:fkrull/deadsnakes-python2.7 -y
+sudo apt-get install libpython2.7-dev python-numpy -y
 sudo apt-get install python2.7 -y
 sudo apt-get install python-pip -y
 sudo pip install docker 
