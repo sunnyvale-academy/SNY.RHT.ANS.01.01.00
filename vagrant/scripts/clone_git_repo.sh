30b58b3b153d4f3dc5828f208c63f44e8b2c6dac
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-01-21 22:52:17 +0100

    fix

--- vagrant/scripts/clone_git_repo.sh
+++ vagrant/scripts/clone_git_repo.sh
@@ -1,4 +1,4 @@
 
 sudo -u ansible rm -rf /home/ansible/git
 sudo -u ansible mkdir /home/ansible/git
-sudo -u ansible -H git clone https://github.com/sunnyvale-academy/SNY.OSS.ANS.01.01.00.git /home/ansible/git/SNY.OSS.ANS.01.01.00
+sudo -u ansible -H git clone https://github.com/sunnyvale-academy/SNY.RHT.ANS.01.01.00.git /home/ansible/git/SNY.RHT.ANS.01.01.00
