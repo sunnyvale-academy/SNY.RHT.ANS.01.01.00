4901fdba2c48c608a862ee514b1c59c83acb5dfd
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-11-27 01:25:04 +0100

    fix

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,8 +7,6 @@ variable "REGION" {
 variable "IMAGE" {
     type="map"
     default={
-        "us-west1-a"="debian-cloud/debian-9"
-        "us-west1-b"="debian-cloud/debian-9"
-        "us-west2-a"="debian-cloud/debian-8"
+        "europe-west1-b"="debian-cloud/debian-9"
     }
 }
