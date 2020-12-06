6acac4e4f1d4d2d9df50445d86ec987e9d621e52
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-12-06 19:34:05 +0100

    ubuntu bionic

--- vagrant/scripts/install_control01_software.sh
+++ vagrant/scripts/install_control01_software.sh
@@ -1,8 +1,12 @@
 sudo apt-get install tree -y
 sudo apt-get install curl -y 
-sudo  add-apt-repository ppa:fkrull/deadsnakes -y
-sudo apt-get install libpython-dev python-numpy -y
-sudo apt-get install python -y
-sudo apt-get install python-pip -y
-sudo pip install docker 
-sudo pip install 'docker-compose>=1.7.0'
+#sudo  add-apt-repository ppa:fkrull/deadsnakes -y
+#sudo apt-get install libpython-dev python-numpy -y
+#sudo apt-get install python -y
+#sudo apt-get install python-pip -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update
+sudo apt-get install python3.5 -y
+sudo apt install python3-pip -y
+sudo pip3 install docker 
+sudo pip3 install 'docker-compose>=1.7.0'
