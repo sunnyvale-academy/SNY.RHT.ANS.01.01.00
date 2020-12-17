2dafb5690eab623771658ad88ebb0d50715211bd
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2020-12-17 14:24:20 +0100

    fix

--- vagrant/scripts/install_ansible.sh
+++ vagrant/scripts/install_ansible.sh
@@ -3,6 +3,8 @@ sudo add-apt-repository -r --yes ppa:ansible/ansible
 sudo add-apt-repository --yes ppa:ansible/ansible-2.9
 sudo apt-get update
 sudo apt-get install --yes ansible
+sudo apt-get install --yes python-pip
+pip install requests
 #sudo apt-get upgrade --yes ansible
 
 
