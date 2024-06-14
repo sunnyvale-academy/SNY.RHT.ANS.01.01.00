607a1bd2bddd243107af4f322b25b595ec367eba
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2024-06-14 09:23:20 +0200

    fix

--- ansible/labs/cicd/terraform/config.tf
+++ ansible/labs/cicd/terraform/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket  = "tf-state-demo-ansible" // bucket has to be created manually using GCP dashboard before running this demo, variables can not be used here
+    bucket  = "tf-state-demo-ansible1" // bucket has to be created manually using GCP dashboard before running this demo, variables can not be used here
     prefix  = "terraform/state" // variables can not be used here
-    //credentials = "/home/vagrant/SNY-OSS-TRF-01-01-00-870577b1e676.json" // variables can not be used here
+    credentials = "/home/vagrant/credentials.json" // variables can not be used here
   }
 }
